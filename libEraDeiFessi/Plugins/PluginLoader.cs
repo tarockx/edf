b55b83de0994b4ba3322fa3b4259c42f9905a997
Elia Dal Santo <Elia.DalSanto.GE1418@infocamere.it>
2016-08-26 15:35:05 +0200

    Removed logging from shared library, since it broke compilation

--- libEraDeiFessi/Plugins/PluginLoader.cs
+++ libEraDeiFessi/Plugins/PluginLoader.cs
@@ -1,5 +1,4 @@
-﻿using log4net;
-using System;
+﻿using System;
 using System.IO;
 using System.Reflection;
 
@@ -7,7 +6,6 @@ namespace libEraDeiFessi.Plugins
 {
     public static class PluginLoader
     {
-        private static ILog Logger { get; set; } = LogManager.GetLogger(typeof(PluginLoader));
 
         public static void LoadPlugins(string path)
         {
@@ -18,7 +16,6 @@ namespace libEraDeiFessi.Plugins
                     if (!file.Contains("EDFPlugin"))
                         continue;
 
-                    Logger.Debug("Trying to load plugin assembly from file: " + file);
                     Assembly a = Assembly.LoadFrom(file);
                     foreach (Type t in a.GetTypes())
                     {
@@ -26,14 +23,11 @@ namespace libEraDeiFessi.Plugins
                         {
                             try
                             {
-                                Logger.Debug("EDF Plugin Interface found in type " + t.Name + ", trying to instantiate...");
                                 IEDFPlugin pluginclass = Activator.CreateInstance(t) as IEDFPlugin;
                                 PluginsRepo.Plugins.Add(pluginclass.pluginID, pluginclass);
-                                Logger.Debug("Plugin " + t.Name + " instantiated succesfully!");
                             }
                             catch (Exception)
                             {
-                                Logger.Debug("Plugin " + t.Name + " failed to instantiate!");
                             }
                         }
                     }
@@ -41,7 +35,6 @@ namespace libEraDeiFessi.Plugins
             }
             catch (Exception ex)
             {
-                Logger.Debug("Exception in LoadPlugins(): " + ex.Message, ex);
                 throw ex;
             }
         }
